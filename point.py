01b9846e95608e0db4a38acb2348fb986f266fba
Luka140 <luka-groot@hotmail.nl>
2023-06-02 15:53:46 +0200

    function to write points to file

--- point.py
+++ point.py
@@ -12,6 +12,13 @@ class Point:
     z: float;
     t: float;
 
+    def __eq__(self, other):
+        if type(other) == Point:
+            return self.label == other.label
+        if type(other) == str:
+            return self.label == other
+        raise NotImplementedError
+
 
 def load_points_from_file(filename: str) -> list[Point]:
     out_list: list
@@ -22,3 +29,10 @@ def load_points_from_file(filename: str) -> list[Point]:
     for i in range(entry_count):
         out_list[i] = Point(data_in["point label"][i], data_in["x"][i], data_in["y"][i], data_in["z"][i], data_in["T"][i])
     return out_list
+
+
+def write_points_to_file(filename: str, points: list[Point]):
+    with open(filename, 'w') as f:
+        f.write('point label, x, y, z, T\n')
+        for point in points:
+            f.write(f"{point.label},{str(point.x)},{str(point.y)},{str(point.z)},{str(point.t)}\n")
